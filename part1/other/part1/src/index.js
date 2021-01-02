9e5001eb21dba6f4d11c97d2a4bbf3485566da9f
Philipp Tobias Christian Kant <ptckant@gmail.com>
2021-01-02 13:07:38 +0100

    lecture 1d.rules_of_hooks done

--- part1/other/part1/src/index.js
+++ part1/other/part1/src/index.js
@@ -1,45 +1,53 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const App = () => {
-  const [clicks, setClicks] = useState({
-    left: 0, right: 0
-  })
-
-  const handleLeftClick = () =>
-    setClicks({ ...clicks, left: clicks.left + 1 })
-  
-  const handleRightClick = () =>
-    setClicks({ ...clicks, right: clicks.right + 1 })
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
 
   return (
     <div>
-      {clicks.left}
-      <button onClick={handleLeftClick}>left</button>
-      <button onClick={handleRightClick}>right</button>
-      {clicks.right}
+      button press history: {props.allClicks.join(' ')}
     </div>
   )
 }
 
-// Sometimes many state variables are better than storing all data in one:
-// const App = () => {
-//   const [left, setLeft] = useState(0)
-//   const [right, setRight] = useState(0)
+const Button = ({ onClick, text }) => (
+  <button onClick={onClick}>
+    {text}
+  </button>
+)
 
-//   return (
-//     <div>
-//       {left}
-//       <button onClick={() => setLeft(left + 1)}>
-//         left
-//       </button>
-//       <button onClick={() => setRight(right + 1)}>
-//         right
-//       </button>
-//       {right}
-//     </div>
-//   )
-// }
+const App = () => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+  }
+
+  return (
+    <div>
+      {left}
+      <Button onClick={handleLeftClick} text='left' />
+      <Button onClick={handleRightClick} text='right' />
+      {right}
+      <History allClicks={allClicks} />
+    </div>
+  )
+}
 
 
 ReactDOM.render(

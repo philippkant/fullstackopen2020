952976ed1a7bbc2a6343c8200951bc7ffa117abc
Philipp Tobias Christian Kant <ptckant@gmail.com>
2021-01-02 12:06:46 +0100

    lecture 1d.complex state done

--- part1/other/part1/src/index.js
+++ part1/other/part1/src/index.js
@@ -1,39 +1,45 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Display = ({ counter }) => <div>{counter}</div>
-
-const Button = ({ handleClick, text }) => (
-  <button onClick={handleClick}>
-    {text}
-  </button>
-)
-
 const App = () => {
-  const [ counter, setCounter ] = useState(0)
+  const [clicks, setClicks] = useState({
+    left: 0, right: 0
+  })
 
-  const increaseByOne = () => setCounter(counter + 1)
-  const decreaseByOne = () => setCounter(counter - 1)
-  const setToZero = () => setCounter(0)
+  const handleLeftClick = () =>
+    setClicks({ ...clicks, left: clicks.left + 1 })
+  
+  const handleRightClick = () =>
+    setClicks({ ...clicks, right: clicks.right + 1 })
 
   return (
     <div>
-      <Display counter={counter}/>
-      <Button
-        handleClick={increaseByOne}
-        text='plus'
-      />
-      <Button
-        handleClick={setToZero}
-        text='zero'
-      />     
-      <Button
-        handleClick={decreaseByOne}
-        text='minus'
-      />   
+      {clicks.left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {clicks.right}
     </div>
   )
 }
+
+// Sometimes many state variables are better than storing all data in one:
+// const App = () => {
+//   const [left, setLeft] = useState(0)
+//   const [right, setRight] = useState(0)
+
+//   return (
+//     <div>
+//       {left}
+//       <button onClick={() => setLeft(left + 1)}>
+//         left
+//       </button>
+//       <button onClick={() => setRight(right + 1)}>
+//         right
+//       </button>
+//       {right}
+//     </div>
+//   )
+// }
 
 
 ReactDOM.render(
